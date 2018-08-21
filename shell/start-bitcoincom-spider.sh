2c44597155161bb1885c14ab2c513164b871f2d1
hxh_robb <hxh.robb@gmail.com>
2018-08-21 17:00:12 +0800

    Update start shell script

--- shell/start-bitcoincom-spider.sh
+++ shell/start-bitcoincom-spider.sh
@@ -11,6 +11,7 @@ mkdir -p .docker
 
 ## Fetch interval from arguments
 test -z $1 && PARAM='1' || PARAM=$1
+test "$PARAM" -lt '60' && PARAM='60'
 PARAM="$PARAM""s"
 
-docker run -d --rm --name "$SOURCE""-spider-$PARAM" -v "$(pwd)/.docker/":/out crypto-trading-volume-spider:latest python /app/$SOURCE.py $@
+docker run -d --rm --name "scheduled-$SOURCE""-spider-$PARAM" -v "$(pwd)/.docker/":/out crypto-trading-volume-spider:latest python /app/$SOURCE.py $@
